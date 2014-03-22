f6fe32da8fdcbebde475aa38fd10c2370dcac8f9
Finn <finn@finn.io>
2014-03-22 15:40:14 -0700

    Added a status dashboard page

---
+++ assets/js/dashboard.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+    $(".dashboard").text("Every day I'm bufferin...");
+    $.get("/status/status.json").success(function(hosts) {
+        console.log(hosts);
+        $(".dashboard").text("");
+        for(host in hosts) {
+            if(hosts.hasOwnProperty(host)) {
+                var hostbox = $("<div>")
+                    .attr("id", host)
+                    .addClass("host")
+                    .addClass(host)
+                    .append("<b>" + host + "</b>");
+                for(service in hosts[host]) {
+                    if(hosts[host].hasOwnProperty(service)) {
+                        console.log("Setting up box for ", host, service);
+                        var servicebox = $("<div>")
+                            .attr("id", host + "-" + service)
+                            .addClass("service")
+                            .addClass(host)
+                            .addClass(service)
+                            .text(service);
+                        switch(hosts[host][service]['current_state']) {
+                            case "0":
+                                servicebox.addClass("status-okay");
+                                break;
+                            case "1":
+                                servicebox.addClass("status-warning");
+                                break;
+                            case "2":
+                                servicebox.addClass("status-critical");
+                                break;
+                            default:
+                                servicebox.addClass("status-unknown");
+                                break;
+                        }
+                        hostbox.append(servicebox);
+                    }
+                }
+                $(".dashboard").append(hostbox);
+            }
+        }
+    }).error(function() {
+        $(".dashboard").text("#fail");
+    })
+})
