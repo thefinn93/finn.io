9663a1fd0eb991200169fec98b3dd98932ee5c6b
Finn <finn@finn.io>
2016-02-08 16:34:44 -0800

    Instaclick is silly, delete it

--- _assets/javascripts/finn.io.js
+++ _assets/javascripts/finn.io.js
@@ -1,8 +1,3 @@
-InstantClick.init(50);
-$(document).ready(function(){
-  $('#sidebar nav ul').mobileMenu({'topOptionText': 'Menu', 'prependTo': '#sidebar nav'});
-});
-
 var _paq = _paq || [];
 //_paq.push(["trackPageView"]);
 _paq.push(["enableLinkTracking"]);
@@ -13,7 +8,3 @@ _paq.push(["enableLinkTracking"]);
     var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
     g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
 })();
-
-InstantClick.on('change', function() {
-  _paq.push(['trackPageView', location.pathname + location.search]);
-});
