b24a8206ddf8c989a718cccf7addfd8b6419bb00
Finn <finn@finn.io>
2016-06-08 22:42:39 -0700

    Re-add analytics
    for your protection

--- _assets/javascripts/finn.io.js
+++ _assets/javascripts/finn.io.js
@@ -1,10 +1,10 @@
-// var _paq = _paq || [];
-// //_paq.push(["trackPageView"]);
-// _paq.push(["enableLinkTracking"]);
-// (function() {
-//     var u=(("https:" == document.location.protocol) ? "https" : "http") + "://analytics.finn.io/";
-//     _paq.push(["setTrackerUrl", u+"piwik.php"]);
-//     _paq.push(["setSiteId", "15"]);
-//     var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-//     g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
-// })();
+var _paq = _paq || [];
+//_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
+(function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://a.finn.io/";
+    _paq.push(["setTrackerUrl", u+"piwik.php"]);
+    _paq.push(["setSiteId", "2"]);
+    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+})();
